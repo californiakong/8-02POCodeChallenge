be189e9a66f7254e4646796098a188ad9945d250
Bryce Zimbelman <bryce.zimbelman@snhu.edu>
2021-08-06 13:38:08 -0500

    Solved Problem 1

---
+++ codechallenge/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace codechallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = "Minneapolis";
+            string s2 = "Minnesota";
+            int common = 0;
+            List<int> commonSubSequence = new List<int>();
+
+            for (int i = 0; i < s1.Length && i < s2.Length; i++) {
+                if (s1[i] == s2[i]) {
+                    common++;
+                } else {
+                    commonSubSequence.Add(common);
+                    common = 0;
+                }   
+            }
+            commonSubSequence.Add(common);
+            Console.WriteLine(commonSubSequence.Max());
+        }
+    }
+}
