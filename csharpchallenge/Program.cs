0153d52ef6518ace95345c782bce1e5ea944c59b
Bryce Zimbelman <bryce.zimbelman@snhu.edu>
2021-08-10 12:46:43 -0500

    Added csharp challenge

---
+++ csharpchallenge/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace csharpchallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int b = 0;
+            int m = 2;
+            int n = 5;
+
+            Console.WriteLine((Math.Pow(n, b + 1) - Math.Pow(m, b + 1)));
+        }
+    }
+}
